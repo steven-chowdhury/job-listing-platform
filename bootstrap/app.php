67f0e98944cc823cf98573a8552fb3f4cced2250
Steven Chowdhury <steven.chowdhury@gmail.com>
2025-04-23 21:36:52 -0700

    Add exception handling middleware for consistent JSON responses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -20,5 +23,22 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ValidationException $e) {
+            return response()->json([
+                'message' => 'Validation Error',
+                'fields' => $e->errors()
+            ], 400);
+        });
+
+        $exceptions->render(function (HttpException $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+            ], $e->getStatusCode());
+        });
+
+        $exceptions->render(function (Throwable $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        });
     })->create();
